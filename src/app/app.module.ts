a6d389ca4d1ed04262a6e8c3ed205feea0129382
Keshav Reddy <keshavlingala@gmail.com>
2019-09-12 16:07:17 +0530

    Dependencies Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFirePerformanceModule} from '@angular/fire/performance';
+import {AngularFireMessagingModule} from '@angular/fire/messaging';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule, MatBottomSheetModule,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule, MatListModule, MatMenuModule, MatNativeDateModule, MatProgressBarModule, MatProgressSpinnerModule,
+  MatRadioModule, MatSelectModule, MatSnackBarModule, MatTabsModule, MatToolbarModule, MatTooltipModule
+} from '@angular/material';
+import {ToastrModule, ToastrService} from 'ngx-toastr';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +37,52 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFirestoreModule.enablePersistence({
+      synchronizeTabs: true
+    }),
+    // TODO add firebase config in environment firebase
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirePerformanceModule,
+    AngularFireMessagingModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatBottomSheetModule,
+    ToastrModule.forRoot(),
+    HttpClientModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ToastrService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
